fb3ef828eedf6f77bf1e13d53da59103b3f8117a
savagebananas <bruno7405@gmail.com>
2026-02-18 00:21:48 -0600

    Started prototype of settings menu UI

---
+++ Project Mist/Assets/_General/SettingsManager/Settings.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    [SerializeField] float sensitivity;
+    [SerializeField] float volume;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
